fa6194da2d3140ce55f5c68235030cca9630fb28
MinisGimis <minisgimis@gmail.com>
2023-03-01 01:55:27 -0500

    made changes

--- src/Grid.js
+++ src/Grid.js
@@ -6,7 +6,7 @@ const COLUMNS = 5;
 
 function getRandomInt(max) {
   let retVal = Math.floor(Math.random() * max)
-  //console.log("random is " + retVal)
+  ////console.log("random is " + retVal)
   return retVal;
 }
 
@@ -32,7 +32,7 @@ function App() {
   const [lockedValue, setLockedValue] = useState(0)
 
   const resetGame = () => {
-    console.log('reset')
+    //console.log('reset')
     setInventoryGrid(Array(3).fill('empty'))
     setPlayGrid(Array(25).fill('empty'))
     setScore(0)
@@ -64,16 +64,16 @@ function App() {
   
   const handleDrop = (e, index) => {
     e.preventDefault();
-    console.log(e.target)
+    //console.log(e.target)
 
-    if (e.target.id != "empty") {
-      console.log("empty");
+    if (e.target.id !== "empty") {
+      //console.log("empty");
     }
-    else if (getColumn(index) == lockedColumn && getRow(index) == lockedRow) {
-      console.log("locked")
+    else if (getColumn(index) === lockedColumn && getRow(index) === lockedRow) {
+      //console.log("locked")
     }
-    else if (getColumn(index) == lockedColumn2 && getRow(index) == lockedRow2) {
-      console.log("locked")
+    else if (getColumn(index) === lockedColumn2 && getRow(index) === lockedRow2) {
+      //console.log("locked")
     }
     else {
       setInventoryGrid(prevGrid => {
@@ -102,7 +102,7 @@ function App() {
       }
       return newGrid
     })
-    console.log("vertical cleared")
+    //console.log("vertical cleared")
   }
 
   const clearHorizontal = (index) => {
@@ -113,7 +113,7 @@ function App() {
       }
       return newGrid
     })
-    console.log("horizontal cleared")
+    //console.log("horizontal cleared")
   }
 
   const checkBoard = (index, newColour) => {
@@ -121,9 +121,9 @@ function App() {
     let locked1 = lockedColumn + lockedRow*ROWS
     let locked2 = lockedColumn2 + lockedRow2*ROWS
     let temp = document.getElementsByClassName("locked");
-    console.log("bbbb", temp)
+    //console.log("bbbb", temp)
     for (let i = 0; i < temp.length; i++) {
-      console.log("aaaaaaaaa", temp[i])
+      //console.log("aaaaaaaaa", temp[i])
       if (temp[i]) {
         if (!temp[i].classList.contains(locked1.toString()) && !temp[i].classList.contains(locked2.toString())) {
           temp[i].classList.remove("locked")
@@ -132,8 +132,8 @@ function App() {
     }
     let element1 = document.getElementsByClassName(locked1.toString())
     let element2 = document.getElementsByClassName(locked2.toString())
-    console.log(element1, "element1")
-    console.log(element2, "element2")
+    //console.log(element1, "element1")
+    //console.log(element2, "element2")
     if (element1[0] && !element1[0].classList.contains("locked")) {
       element1[0].classList.add("locked")
     }
@@ -142,29 +142,29 @@ function App() {
     }
 
     let addScore = 0;
-    if (index == null) {
+    if (index === null) {
       return 0
     }
-    if (newColour == "empty") {
+    if (newColour === "empty") {
       return 0
     }
-    console.log(getRow(index), getColumn(index));
+    //console.log(getRow(index), getColumn(index));
 
     //check vertical
     let vertical = true;
     for (let i = getColumn(index); i < ROWS*COLUMNS; i = i+COLUMNS) {
-      if (i != index && playGrid[i] != newColour) {
+      if (i !== index && playGrid[i] !== newColour) {
         vertical = false;
         break;
       }
     }
 
-    console.log("vertical", vertical)
+    //console.log("vertical", vertical)
 
     //check horizontal
     let horizontal = true;
     for (let i = getRow(index)*COLUMNS; i < getRow(index)*COLUMNS + COLUMNS; i++) {
-      if (i != index && playGrid[i] != newColour) {
+      if (i !== index && playGrid[i] !== newColour) {
         horizontal = false;
         break;
       }
@@ -185,7 +185,7 @@ function App() {
       clearHorizontal(index)
       return 1
     }
-    console.log("horizontal", horizontal)
+    //console.log("horizontal", horizontal)
     return 0;
   }
 
@@ -195,7 +195,7 @@ function App() {
       let x2 = getRandomInt(ROWS)
       let y1 = getRandomInt(COLUMNS)
       let y2 = getRandomInt(COLUMNS)
-      while (x1 == x2 && y1 == y2) {
+      while (x1 === x2 && y1 === y2) {
         x1 = getRandomInt(ROWS)
         x2 = getRandomInt(ROWS)
         y1 = getRandomInt(COLUMNS)
@@ -207,31 +207,31 @@ function App() {
       setLockedRow2(x2)
       setLockedValue(lockedValue - 1000)
     }
-    console.log(lockedColumn, lockedRow, lockedValue)
+    //console.log(lockedColumn, lockedRow, lockedValue)
 
-  }, [lockedValue])
+  }, lockedValue)
 
   useEffect(() => {
     let result = 0;
     let failed = true;
-    console.log("checked")
+    //console.log("checked")
     setLockedValue(lockedValue + getRandomInt(500))
     for (let i = 0; i < ROWS*COLUMNS; i++) {
-      if (playGrid[i] == "empty" && !document.getElementsByClassName(i.toString())[0].classList.contains("locked")) {
+      if (playGrid[i] === "empty" && !document.getElementsByClassName(i.toString())[0].classList.contains("locked")) {
         failed = false;
       }
       result = Math.max(result, checkBoard(i, playGrid[i]));
     }
-    if (result == 0 && failed) {
+    if (result === 0 && failed) {
       setLost(true)
     }
-    else if (result == 1) {
+    else if (result === 1) {
       setScore(score + 100)
     }
-    else if (result == 2) {
+    else if (result === 2) {
       setScore(score + 100)
     }
-    else if (result == 3) {
+    else if (result === 3) {
       setScore(score + 500)
     }
   }, inventoryGrid)
@@ -244,13 +244,13 @@ function App() {
       setInventoryGrid(prevGrid => {
         
         return prevGrid.map(square => {
-          //console.log ((Math.log10(score))/100);
-          if (square != "empty") {
+          ////console.log ((Math.log10(score))/100);
+          if (square !== "empty") {
             return square
           }
           
           let roll = Math.random();
-          console.log(0.15 + (Math.log10(score+1))/100)
+          //console.log(0.15 + (Math.log10(score+1))/100)
           if (roll < (Math.log10(score))/100) {
             return 'treasure'
           }
