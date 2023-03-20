a4693a746bd21eb4c9e27bbfb8c808252e0b5d7b
MinisGimis <81587461+MinisGimis@users.noreply.github.com>
2023-03-20 03:23:36 -0400

    fixed lock bug, added 2x2 square matching

--- src/Grid.js
+++ src/Grid.js
@@ -29,7 +29,7 @@ function App() {
   const [lockedRow, setLockedRow] = useState(getRandomInt(ROWS));
   const [lockedColumn2, setLockedColumn2] = useState(getRandomInt(COLUMNS));
   const [lockedRow2, setLockedRow2] = useState(getRandomInt(ROWS));
-  const [lockedValue, setLockedValue] = useState(0)
+  const [lockedValue, setLockedValue] = useState(1001)
 
   const resetGame = () => {
     ////console.log('reset')
@@ -61,34 +61,62 @@ function App() {
       e.preventDefault();
     }
   };
+
+  const fillInventory = () => {
+    let roll = Math.random();
+    ////console.log(0.15 + (Math.log10(score+1))/100)
+    if (roll < (Math.log10(score))/100) {
+      return 'treasure'
+    }
+    else if (roll < (0.15 + (Math.log10(score+1))/100)) {
+      return 'red';
+    }
+    else if (roll < 0.35 + (Math.log10(score+1))/100) {
+      return 'green';
+    }
+    else if (roll < 0.55 + (Math.log10(score+1))/100) {
+      return 'yellow';
+    }
+    else if (roll < 0.75 + (Math.log10(score+1))/100) {
+      return 'purple';
+    }
+    else {
+      return 'blue';
+    }
+  }
   
   const handleDrop = (e, index) => {
     e.preventDefault();
     ////console.log(e.target)
 
     if (e.target.id !== "empty") {
-      ////console.log("empty");
+      //console.log("not empty!");
     }
     else if (getColumn(index) === lockedColumn && getRow(index) === lockedRow) {
-      ////console.log("locked")
+      //console.log("locked")
     }
     else if (getColumn(index) === lockedColumn2 && getRow(index) === lockedRow2) {
-      ////console.log("locked")
+      //console.log("locked")
     }
     else {
+      let targetColour = draggedStart.id
+      //console.log("draggedStart", draggedStart)
+      //console.log(targetColour)
       setInventoryGrid(prevGrid => {
         const newGrid = [...prevGrid];
-        newGrid[draggedStartIndex] = "empty"
+        newGrid[draggedStartIndex] = fillInventory();
+        //console.log(newGrid[draggedStartIndex])
         return newGrid;
       });
-      e.target.id = draggedStart.id
-      draggedStart.id = "empty"
 
-      setPlayGrid(prevGrid => {
-        const newGrid = [...prevGrid]
-        newGrid[index] = e.target.id
-        return newGrid
-      });
+      //e.target.id = targetColour
+      //draggedStart.id = "empty"
+      
+      //f (targetColour != "empty") {
+        checkBoard(index, targetColour)
+        updateLocks()
+        //console.log("board checked")
+      //}
     }
     setDraggedStart(null)
     setDraggedStartIndex(null)
@@ -116,24 +144,93 @@ function App() {
     ////console.log("horizontal cleared")
   }
 
-  const checkBoard = (index, newColour) => {
+  const clearSE = (index) => {
+    setPlayGrid(prevGrid => {
+      const newGrid = [...prevGrid]
+      playGrid[index] = "empty"
+      playGrid[index+1] = "empty"
+      playGrid[index+ROWS] = "empty"
+      playGrid[index+ROWS+1] = "empty"
+      return newGrid
+    })
+  }
 
-    let locked1 = lockedColumn + lockedRow*ROWS
-    let locked2 = lockedColumn2 + lockedRow2*ROWS
-    let temp = document.getElementsByClassName("locked");
+  const clearSW = (index) => {
+    setPlayGrid(prevGrid => {
+      const newGrid = [...prevGrid]
+      playGrid[index] = "empty"
+      playGrid[index-1] = "empty"
+      playGrid[index+ROWS] = "empty"
+      playGrid[index+ROWS-1] = "empty"
+      return newGrid
+    })
+  }
+
+  const clearNE = (index) => {
+    setPlayGrid(prevGrid => {
+      const newGrid = [...prevGrid]
+      playGrid[index] = "empty"
+      playGrid[index+1] = "empty"
+      playGrid[index-ROWS] = "empty"
+      playGrid[index-ROWS+1] = "empty"
+      return newGrid
+    })
+  }
+
+  const clearNW = (index) => {
+    setPlayGrid(prevGrid => {
+      const newGrid = [...prevGrid]
+      playGrid[index] = "empty"
+      playGrid[index-1] = "empty"
+      playGrid[index-ROWS] = "empty"
+      playGrid[index-ROWS-1] = "empty"
+      return newGrid
+    })
+  }
+
+  const updateLocks = () => {
+    
+    let val = lockedValue
+    //console.log("updateLocks called", val)
+
+    let x1 = lockedRow
+    let x2 = lockedRow2
+    let y1 = lockedColumn
+    let y2 = lockedColumn2
+
+    if (val > 1000) {
+      val = 0;
+      x1 = getRandomInt(ROWS)
+      x2 = getRandomInt(ROWS)
+      y1 = getRandomInt(COLUMNS)
+      y2 = getRandomInt(COLUMNS)
+      while (x1 === x2 && y1 === y2) {
+        x1 = getRandomInt(ROWS)
+        x2 = getRandomInt(ROWS)
+        y1 = getRandomInt(COLUMNS)
+        y2 = getRandomInt(COLUMNS)
+      }
+      setLockedColumn(y1)
+      setLockedRow(x1)
+      setLockedColumn2(y2)
+      setLockedRow2(x2)
+    }
+    
+    let locked1 = y1 + x1*ROWS
+    let locked2 = y2 + x2*ROWS
+    let temp = document.getElementsByClassName("square");
+    //console.log(temp)
     ////console.log("bbbb", temp)
     for (let i = 0; i < temp.length; i++) {
       ////console.log("aaaaaaaaa", temp[i])
       if (temp[i]) {
-        if (!temp[i].classList.contains(locked1.toString()) && !temp[i].classList.contains(locked2.toString())) {
+        //if (!temp[i].classList.contains(locked1.toString()) && !temp[i].classList.contains(locked2.toString())) {
           temp[i].classList.remove("locked")
-        }
+        //}
       }
     }
     let element1 = document.getElementsByClassName(locked1.toString())
     let element2 = document.getElementsByClassName(locked2.toString())
-    ////console.log(element1, "element1")
-    ////console.log(element2, "element2")
     if (element1[0] && !element1[0].classList.contains("locked")) {
       element1[0].classList.add("locked")
     }
@@ -141,12 +238,17 @@ function App() {
       element2[0].classList.add("locked")
     }
 
+    setLockedValue(val + getRandomInt(499))
+  }
+
+  const checkBoard = (index, newColour) => {
     if (index === null) {
       return 0
     }
     if (newColour === "empty") {
       return 0
     }
+
     ////console.log(getRow(index), getColumn(index));
 
     //check vertical
@@ -168,114 +270,118 @@ function App() {
         break;
       }
     }
+    let i1, i2, i3 = 0
+    let squareSE = false;
+    i1 = index+1
+    i2 = index+5
+    i3 = index+6
+    if (i1 >= 0 && i2 >= 0 && i3 >= 0 && i1 < COLUMNS*ROWS && i2 < COLUMNS*ROWS && i3 < COLUMNS*ROWS) {
+      if (playGrid[i1] == newColour && playGrid[i2] == newColour && playGrid[i3] == newColour) {
+        squareSE = true
+        //console.log("squareSE!")
+      }
+    }
+
+    i1 = index-1
+    i2 = index+4
+    i3 = index+5
+    let squareSW = false;
+    if (i1 >= 0 && i2 >= 0 && i3 >= 0 && i1 < COLUMNS*ROWS && i2 < COLUMNS*ROWS && i3 < COLUMNS*ROWS) {
+      if (playGrid[i1] == newColour && playGrid[i2] == newColour && playGrid[i3] == newColour) {
+        squareSW = true
+        //console.log("squareSW!")
+      }
+    }
+
+
+    i1 = index+1
+    i2 = index-5
+    i3 = index-4
+    let squareNE = false;
+    if (i1 >= 0 && i2 >= 0 && i3 >= 0 && i1 < COLUMNS*ROWS && i2 < COLUMNS*ROWS && i3 < COLUMNS*ROWS) {
+      if (playGrid[i1] == newColour && playGrid[i2] == newColour && playGrid[i3] == newColour) {
+        squareNE = true
+        //console.log("squareNE!")
+      }
+    }
+
+    i1 = index-1
+    i2 = index-5
+    i3 = index-6
+    let squareNW = false;
+    if (i1 >= 0 && i2 >= 0 && i3 >= 0 && i1 < COLUMNS*ROWS && i2 < COLUMNS*ROWS && i3 < COLUMNS*ROWS) {
+      if (playGrid[i1] == newColour && playGrid[i2] == newColour && playGrid[i3] == newColour) {
+        squareNW = true
+        //console.log("squareNW!")
+      }
+    }
+
+    let bonus = 0;
+    if (squareNE) {
+      clearNE(index)
+      bonus += 100
+    }
+
+    if (squareNW) {
+      clearNW(index)
+      bonus += 100
+    }
+
+    if (squareSE) {
+      clearSE(index)
+      bonus += 100
+    }
+
+    if (squareSW) {
+      clearSW(index)
+      bonus += 100
+    }
 
     if (horizontal && vertical) {
       clearHorizontal(index)
       clearVertical(index)
-      return 3
+      bonus += 500
     }
 
-    if (vertical) {
+    else if (vertical) {
       clearVertical(index)
-      return 2
+      bonus += 100
     }
 
-    if (horizontal) {
+    else if (horizontal) {
       clearHorizontal(index)
-      return 1
+      bonus += 100
     }
+    if (bonus <= 0) {
+      setPlayGrid(prevGrid => {
+        const newGrid = [...prevGrid]
+        newGrid[index] = newColour
+        return newGrid
+      });
+    }
+    setScore(score + bonus)
     ////console.log("horizontal", horizontal)
     return 0;
   }
 
   useEffect(() => {
-    if (lockedValue > 1000) {
-      let x1 = getRandomInt(ROWS)
-      let x2 = getRandomInt(ROWS)
-      let y1 = getRandomInt(COLUMNS)
-      let y2 = getRandomInt(COLUMNS)
-      while (x1 === x2 && y1 === y2) {
-        x1 = getRandomInt(ROWS)
-        x2 = getRandomInt(ROWS)
-        y1 = getRandomInt(COLUMNS)
-        y2 = getRandomInt(COLUMNS)
-      }
-      setLockedColumn(y1)
-      setLockedRow(x1)
-      setLockedColumn2(y2)
-      setLockedRow2(x2)
-      setLockedValue(lockedValue - 1000)
-    }
-    //console.log(lockedColumn, lockedRow, lockedValue)
-
-  }, [lockedValue])
-
-  useEffect(() => {
-    let result = 0;
-    let failed = true;
-    //console.log("checked")
-    setLockedValue(lockedValue + getRandomInt(500))
-    for (let i = 0; i < ROWS*COLUMNS; i++) {
-      if (playGrid[i] === "empty" && !document.getElementsByClassName(i.toString())[0].classList.contains("locked")) {
-        failed = false;
-      }
-      result = Math.max(result, checkBoard(i, playGrid[i]));
-    }
-    if (result === 0 && failed) {
-      setLost(true)
-    }
-    else if (result === 1) {
-      setScore(score + 100)
-    }
-    else if (result === 2) {
-      setScore(score + 100)
-    }
-    else if (result === 3) {
-      setScore(score + 500)
-    }
-  }, inventoryGrid)
-
-  useEffect(() => {
-    let intervalId = null;
-    
-    const updateGrid = () => {
-
-      setInventoryGrid(prevGrid => {
-        
-        return prevGrid.map(square => {
-          //////console.log ((Math.log10(score))/100);
-          if (square !== "empty") {
-            return square
-          }
-          
-          let roll = Math.random();
-          ////console.log(0.15 + (Math.log10(score+1))/100)
-          if (roll < (Math.log10(score))/100) {
-            return 'treasure'
-          }
-          else if (roll < (0.15 + (Math.log10(score+1))/100)) {
-            return 'red';
-          }
-          else if (roll < 0.35 + (Math.log10(score+1))/100) {
-            return 'green';
-          }
-          else if (roll < 0.55 + (Math.log10(score+1))/100) {
-            return 'yellow';
-          }
-          else if (roll < 0.75 + (Math.log10(score+1))/100) {
-            return 'purple';
-          }
-          else {
-            return 'blue';
-          }
+    //console.log("checking inventory", inventoryGrid)
+    for (let i = 0; i < inventoryGrid.length; i++) {
+      if (inventoryGrid[i] == "empty") {
+        setInventoryGrid(prevGrid => {
+          const newGrid = [...prevGrid];
+          newGrid[i] = fillInventory();
+          return newGrid;
         });
-      });
-      intervalId = setTimeout(updateGrid, 100);
-    };
+      }
+    }
+
+  }, [inventoryGrid])
+
+  useEffect (() => {
+    updateLocks()
+  }, [])
   
-    updateGrid(); // trigger the first interval immediately
-    return () => clearTimeout(intervalId);
-  }, []);
 
   return (
     <div>
@@ -302,7 +408,7 @@ function App() {
             <div
               key={index}
               id={square}
-              className={`${`square`}`}
+              className={`${`square inventory`}`}
               draggable = {true}
               onDragEnd={(e) => handleDragEnd(e)}
               onDragStart={(e) => handleDragStart(e, index)}
